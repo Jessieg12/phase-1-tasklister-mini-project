224d2299f39cf46994fe41b64670dc4fae6fc79b
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-22 17:30:59 -0600

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleCreation(e.target['new-task-description'].value)
+    form.reset()
+  })
+})
+
+const handleCreation = (value) => {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  p.textContent = `${value} `
+  p.style.color = 'blue'
+  btn.textContent = ' X '
+  btn.style.color = 'red'
+  btn.addEventListener('click', handleDelete)
+  document.querySelector('#tasks').appendChild(p)
+  p.appendChild(btn)
+}
+
+const handleDelete = (e) => {
+  e.target.parentNode.remove()
+}
